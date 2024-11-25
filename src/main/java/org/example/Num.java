e0b624b0b64c3a97d265789038944490c3c1cb5d
apicela <trab.jamilsouza@gmail.com>
2024-11-25 17:32:51 -0300

    first push

---
+++ src/main/java/org/example/Num.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Num extends Token{
+    public final int value;
+    public Num(int value){
+        super(Tag.NUM);
+        this.value = value;
+    }
+    public String toString(){
+        return "" + value;
+    }
+}
