3297d822223327a49b0146c0794ef4dee79be1f5
Jamil Apicela <trab.jamilsouza@gmail.com>
2025-02-22 00:49:46 -0300

    update

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,22 +1,30 @@
 package org.example;
 
+import java.util.List;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         // Verifica se o nome do arquivo foi passado como argumento
+        /*
         if (args.length < 1) {
             System.out.println("Por favor, forneça o nome do arquivo como argumento.");
             return;
         }
-
+*/
 
         // O nome do arquivo é o primeiro argumento passado
-        String caminhoArquivo = args[0];
-
+      //  String caminhoArquivo = args[0];
+        String caminhoArquivo = "C:\\Users\\jamilzin\\compiler\\compiler_jar\\teste01.txt";
         try {
             Lexer lexer = new Lexer(caminhoArquivo);
-            lexer.processTokens();
+            List<Token> tokens = lexer.processTokens();
+            for(Token t : tokens){
+                System.out.println(t);
+            }
+            Parser parser = new Parser(tokens);
+            parser.start();
 
         } catch (Exception e) {
             System.err.println("Erro ao ler o arquivo: " + e.getMessage());
